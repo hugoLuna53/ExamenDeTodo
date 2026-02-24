49ce7cedf56ea8351dd838f2c7c757452c7660ce
Hugo Luna Muñoz <hugolunmu@gmail.com>
2026-02-24 20:07:05 +0100

    me llamo babaa

--- src/modelo/baba.java
+++ src/modelo/baba.java
@@ -2,7 +2,7 @@ package modelo;
 
 public class baba {
     private String nombre,apellidos1;
-    private String nombre,apellidos;
+
     public baba() {
         System.out.println("Hola, soy la clase baba");
 
